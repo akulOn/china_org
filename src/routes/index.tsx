d455c725af1b0072787866ea0c1846c57bc25902
akulOn <dracaluka12@gmail.com>
2025-04-08 23:39:04 +0200

    started working on landing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Paper } from "@mui/material";
 import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -6,17 +7,25 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <>
-      <h1>
-        Welcome to the Home page of:{" "}
-        <span className="p-1 font-bold">CHINA ORG</span>
-      </h1>
-      <div>
-        <span>Please navigate to the items page:</span>
-        <Link className="p-1 font-bold" to={"/items"}>
-          ITEMS
-        </Link>
-      </div>
-    </>
+    <Box
+      className="flex justify-center items-center flex-col gap-5
+        w-full h-full"
+    >
+      <Box
+        className="
+          flex-none
+          inline-grid grid-cols-3 gap-2
+          p-1
+          [&>*]:w-30 [&>*]:h-30"
+      >
+        <Paper />
+        <Paper />
+        <Paper />
+        <Paper />
+        <Paper />
+        <Paper />
+        <Paper />
+      </Box>
+    </Box>
   );
 }
