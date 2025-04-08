4e70a9baf96e839d94896fdb205306fe01e99abb
akulOn <dracaluka12@gmail.com>
2025-04-08 23:38:35 +0200

    added filtering input

--- src/routes/items/index.tsx
+++ src/routes/items/index.tsx
@@ -1,4 +1,7 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { FormControl, Input, InputAdornment, InputLabel } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
 
 export const Route = createFileRoute("/items/")({
   component: RouteComponent,
@@ -6,10 +9,32 @@ export const Route = createFileRoute("/items/")({
 const items = ["Gloves", "Umbrella", "USB", "Glasses", "Knives"];
 
 function RouteComponent() {
+  const [displayedItems, setDisplayedItems] = useState([...items]);
+
   return (
-    <div className="p-2">
-      <div className="grid grid-cols-3 gap-4">
-        {items.map((itemId) => (
+    <div className="flex flex-col gap-5 p-2">
+      <FormControl variant="standard" className="w-150">
+        <InputLabel htmlFor="input-with-icon-adornment">Find item</InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          }
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setDisplayedItems(
+              ...[
+                items.filter((x) =>
+                  x.toLowerCase().includes(event.target.value.toLowerCase())
+                ),
+              ]
+            );
+          }}
+        />
+      </FormControl>
+      <div className="inline-grid grid-cols-3 gap-4">
+        {displayedItems.map((itemId) => (
           <Link
             key={itemId}
             className="p-1 border-1 hover:cursor-pointer"
