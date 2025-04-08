01ff752ac577599a4ad0bb2f8ee70281952b2873
akulOn <dracaluka12@gmail.com>
2025-04-08 23:58:33 +0200

    added links and updated styling

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Paper } from "@mui/material";
+import { Box } from "@mui/material";
 import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -7,25 +7,19 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <Box
-      className="flex justify-center items-center flex-col gap-5
-        w-full h-full"
-    >
-      <Box
-        className="
-          flex-none
-          inline-grid grid-cols-3 gap-2
-          p-1
-          [&>*]:w-30 [&>*]:h-30"
-      >
-        <Paper />
-        <Paper />
-        <Paper />
-        <Paper />
-        <Paper />
-        <Paper />
-        <Paper />
-      </Box>
+    <Box className="flex flex-col justify-center-safe gap-2 bg-amber-100">
+      <span>Welcome. Pick.</span>
+      <div className="flex flex-row gap-2 [&>*]:w-30 [&>*]:h-30">
+        <Link className="bg-amber-50 border rounded-xs" to={"/items"}>
+          <span>Items</span>
+        </Link>
+        <Link className="bg-amber-50 border rounded-xs" to={"/buyers"}>
+          <span>Buyers</span>
+        </Link>
+        <Link className="bg-amber-50 border rounded-xs" to={"/sellers"}>
+          <span>Sellers</span>
+        </Link>
+      </div>
     </Box>
   );
 }
