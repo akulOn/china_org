2ee2525abb89d5738d723a07b4a48a13cfc23daf
akulOn <dracaluka12@gmail.com>
2025-04-11 00:14:44 +0200

    Updates

---
+++ assets/_buyerId-D58dQtMW.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BYknd8Ua.js";const e=function(){return o.jsx("div",{children:'Hello "/buyers/$buyerId"!'})};export{e as component};
---
+++ assets/index-d_LHf4hY.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BYknd8Ua.js";const e=function(){return o.jsx("div",{children:'Hello "/items/$itemId/options/"!'})};export{e as component};
