68455fe6e6b4f161ea1ec7198a3d65e3a3213ae3
akulOn <dracaluka12@gmail.com>
2025-04-26 15:38:59 +0200

    updated main page navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,73 @@
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import { createFileRoute, Link } from "@tanstack/react-router";
+
+import AddIcon from "@mui/icons-material/Add";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PaymentsIcon from "@mui/icons-material/Payments";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
+const links = [
+  {
+    name: "Items",
+    icon: <StorefrontIcon />,
+    to: "/items",
+    desc: "Navigate over different item categories and chose an option",
+  },
+  {
+    name: "Buyers",
+    icon: <PaymentsIcon />,
+    to: "/buyers",
+    desc: "Overview the list of buyers and get details on their orders",
+  },
+  {
+    name: "Sellers",
+    icon: <ShoppingCartIcon />,
+    to: "/sellers",
+    desc: "Analyze all of the current and past sellers",
+  },
+];
+
 function Index() {
   return (
     <Container className="flex flex-col gap-2 items-center h-full">
-      <span className="text-6xl">Welcome. Pick.</span>
-      <Container
-        className="flex flex-col grow-1 gap-2 justify-center items-stretch
-          lg:flex-row lg:items-center lg:*:w-64
-          *:flex *:justify-center *:items-center
-          *:bg-amber-50 *:border
-          *:h-64
-          *:text-6xl"
+      <Typography
+        variant="h1"
+        className="
+          grow-1
+          py-16 lg:py-0
+          lg:flex lg:items-center
+          text-[#B3B7BA] !font-bold"
       >
-        <Link to={"/items"}>
-          <span>Items</span>
-        </Link>
-        <Link to={"/buyers"}>
-          <span>Buyers</span>
-        </Link>
-        <Link to={"/sellers"}>
-          <span>Sellers</span>
-        </Link>
+        Welcome!
+      </Typography>
+      <Container
+        className="flex flex-col grow-2 gap-8 justify-center items-stretch
+          lg:flex-row lg:*:w-64
+          *:h-48
+          *:bg-[#6C6D74] *:shadow-lg *:rounded-4xl
+          text-[#D3D1CE]"
+      >
+        {links.map((link) => (
+          <Link
+            key={link.name}
+            className="flex flex-col justify-between 
+            p-4"
+            to={link.to}
+          >
+            <div className="flex *:!text-4xl">
+              {link.icon}
+              <span>{link.name}</span>
+            </div>
+            <div className="flex">
+              <Typography variant="body2">{link.desc}</Typography>
+              <AddIcon className="!text-5xl" />
+            </div>
+          </Link>
+        ))}
       </Container>
     </Container>
   );
